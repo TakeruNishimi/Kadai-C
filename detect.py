aef66e6ee75e167dcd80b307f321a6cf8eb3bad1
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-25 14:06:41 +0900

    printをクラスのメソッド内に
    コード成形

--- detect.py
+++ detect.py
@@ -3,17 +3,17 @@ class WordFilter:
         self.word =word
 
     def detect(self,sentence):
-        return self.word in sentence
+        print(self.word in sentence)
 
 
 def main():
     my_filter = WordFilter("アーセナル")
 
     # NGワードが含まれている場合
-    print(my_filter.detect("昨日のアーセナルの試合アツかった！"))  # Trueを返す
+    my_filter.detect("昨日のアーセナルの試合アツかった！")  # Trueを返す
 
     # NGワードが含まれていない場合
-    print(my_filter.detect("昨日のリバプールの試合アツかった！")) # Falseを返す
+    my_filter.detect("昨日のリバプールの試合アツかった！")  # Falseを返す
 
 
 if __name__ == '__main__':
