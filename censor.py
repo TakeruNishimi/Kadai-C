bd6bcd9e1d201154991ab1be575773d801ae7b6c
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-25 15:03:32 +0900

    課題C-5 最後にNGワードを変更するか尋ねて、yならばもう一度変更しなおすような仕様

--- censor.py
+++ censor.py
@@ -9,23 +9,35 @@ class WordFilter:
             replace_sentence = replace_sentence.replace(NG_word, self.replace_word)
         print(replace_sentence)
 
+
 def main():
     NG_word_list = []
     while True:
-        print('NGワードを入力してください。')
-        NG_word = input("これ以上必要ないときはそのままエンターを押してください。　＞　")
-        if NG_word == '':
+        while True:
+            print('NGワードを入力してください。')
+            NG_word = input("これ以上必要ないときはそのままエンターを押してください。　＞　")
+            if NG_word == '':
+                break
+            NG_word_list.append(NG_word)
+        replace_word = input("置き換えるワードを入力してください。　＞　")
+
+        my_filter = WordFilter(NG_word_list=NG_word_list, replace_word=replace_word)
+
+        # NGワードが含まれている場合
+        my_filter.censor("昨日のアーセナルの試合アツかった！")
+
+        # NGワードが含まれていない場合
+        my_filter.censor("昨日のリバプールの試合アツかった！")
+
+        while True:
+            change_flag = input('NGワードを変更しますか？　[y/n] ＞　')
+            if change_flag == 'y' or change_flag == 'n':
+                break
+
+        if change_flag == 'y':
+            continue
+        elif change_flag == 'n':
             break
-        NG_word_list.append(NG_word)
-    replace_word = input("置き換えるワードを入力してください。　＞　")
-
-    my_filter = WordFilter(NG_word_list=NG_word_list, replace_word=replace_word)
-
-    # NGワードが含まれている場合
-    my_filter.censor("昨日のアーセナルの試合アツかった！")
-
-    # NGワードが含まれていない場合
-    my_filter.censor("昨日のリバプールの試合アツかった！")
 
 
 if __name__ == '__main__':
